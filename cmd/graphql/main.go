c17eefc61d3eb0889bcc516702696247bd6b0184
wander4747 <wander.douglas14@gmail.com>
2022-06-09 20:08:27 -0300

    config docker

--- cmd/graphql/main.go
+++ cmd/graphql/main.go
@@ -1,1 +1,18 @@
-package graphql
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello devs")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wander4747/adopet-backend
+
+go 1.18
